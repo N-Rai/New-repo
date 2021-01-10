1a1029bc119a376edb0d92808a8e3600bbe5ad0a
Naresh <n.rai@student.fontys.nl>
2021-01-10 19:51:00 +0100

    Fixed Announcement service

--- ITS-Angular-Frontend/src/app/components/announcement/announcement.component.ts
+++ ITS-Angular-Frontend/src/app/components/announcement/announcement.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../service/auth.service';
 import {AnnouncementService} from '../../service/announcement.service';
+import {element} from 'protractor';
 
-export class announcement{
-  topic : string;
+export class announcement {
+  topic: string;
   text: string;
   createdAt: string;
   updatedAt: string;
@@ -17,38 +18,54 @@ export class announcement{
   styleUrls: ['./announcement.component.css']
 })
 export class AnnouncementComponent implements OnInit {
-  announcementData={
+  announcementData = {
     topic: undefined,
     text: undefined
   };
 
   public announcementList = [];
+  public AnnouncementsToDisplay = [];
+  public topicExists: boolean = false;
 
-  constructor( public _announceService: AnnouncementService,
-               public _authService: AuthService) { }
 
-  ngOnInit(): void {
-   this.GetAnnouncements()
-    console.log(this.announcementList);
+  constructor(public _announceService: AnnouncementService,
+              public _authService: AuthService) {
   }
 
-  UploadAnnouncement() {
-    console.log(this.announcementData);
-    this._announceService.PostAnnouncement(this.announcementData)
-      .subscribe(
-        res => {
-          console.log(res);
-        },
-        error => {
-          console.log(error);
-        }
-      );
+  ngOnInit(): void {
+    this.GetAnnouncements();
+  }
+
+  DisplayAnnouncements() {
+    if(this.announcementList.length > 7)
+    this.AnnouncementsToDisplay = this.announcementList.slice( this.announcementList.length - 7, this.announcementList.length);
+    else this.AnnouncementsToDisplay = this.announcementList;
+    console.log("Announcement2display"+this.AnnouncementsToDisplay);
   }
 
   GetAnnouncements() {
     this._announceService.GetAnnouncements()
-      .subscribe(data => this.announcementList = data );
+      .subscribe((data: any[]) => {
+        console.log(data);
+        this.announcementList = data;
+        this.DisplayAnnouncements();
+      });
 
+
+  }
+
+  UploadAnnouncement() {
+    this._announceService.PostAnnouncement(this.announcementData)
+      .subscribe(
+        res => {
+
+        },
+        error => {
+          console.log(error);
+          this.topicExists = error.error.status == 409 ? true : false;
+          this.GetAnnouncements();
+        }
+      );
   }
 
 }
